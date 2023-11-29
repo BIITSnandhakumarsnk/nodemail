419e0ed5327e1b8714cd0a51728fa7ab3bb114a3
BIITSnandhakumarsnk <nandha.kumar@biits.in>
2023-11-29 18:33:05 +0530

    removed personal google credentials

--- app.js
+++ app.js
@@ -1,63 +1,3 @@
-<<<<<<< HEAD
-const express = require("express");
-const nodemailer = require("nodemailer");
-const bodyParser = require("body-parser");
-//test
-const app = express();
-const port = 3000;
-app.use(bodyParser.urlencoded({ extended: true }));
-// Use body-parser middleware to parse JSON requests
-app.use(bodyParser.json());
-
-// Endpoint to receive user details and send email
-app.post("/sendEmail", (req, res) => {
-  const { first_name, last_name, department, email, phone, message } = req.body;
-  require("dotenv").config();
-  const departmentString = department ? department.join(", ") : "";
-  // Create a transporter using SMTP
-  const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com", // Your SMTP server host
-    port: 587, // Your SMTP server port
-    secure: false, // Set to true if your SMTP server requires a secure connection
-    auth: {
-      user: process.env.SMTP_USERNAME, //username
-      pass: process.env.SMTP_PASSWORD, //password
-    },
-    authMethod: "PLAIN", // Specify the authentication method here
-  });
-
-  // Define the email options
-  const mailOptions = {
-    from: email, // Use the user's email as the sender address
-    to: "info@creowiz.com", // Admin's email address
-    subject: "New Student Request",
-    text: `
-      FirstName: ${first_name}
-      LastName:${last_name}
-      Department: ${departmentString}
-      Email: ${email}
-      Phone: ${phone}
-      Message: ${message}
-    `,
-  };
-
-  // Send the email
-  transporter.sendMail(mailOptions, function (error, info) {
-    if (error) {
-      console.error(error);
-      res.status(500).send("Internal Server Error");
-    } else {
-      console.log("Email sent: " + info.response);
-      res.status(200).send("Email sent successfully");
-    }
-  });
-});
-
-// Start the server
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
-=======
 const express = require('express');
 const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
@@ -116,4 +56,3 @@ app.post('/sendEmail', (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
->>>>>>> d2a905fe5ed06f7610e95736cd40ce0f94ae8a02
